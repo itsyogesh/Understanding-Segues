1f1048e882f03d89b67a2a8c7fcd05e50127f1ba
Yogesh Kumar <yogeshkumar180592@gmail.com>
2016-02-16 22:59:36 +0530

    Initial setup for learning segues

---
+++ Segues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Segues
+//
+//  Created by Yogesh Kumar on 16/02/16.
+//  Copyright © 2016 itsyogesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
