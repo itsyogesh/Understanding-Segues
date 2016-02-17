2f2d694638ef593b4adf04b6fb7a507b52574871
Yogesh Kumar <yogeshkumar180592@gmail.com>
2016-02-17 23:41:44 +0530

    connecting segues mannually

--- Segues/ViewController.swift
+++ Segues/ViewController.swift
@@ -14,6 +14,35 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+//    @IBAction func loadBlue(sender: AnyObject!  ){
+//        performSegueWithIdentifier("goToBlue", sender: nil)
+//    }
+    
+    
+    
+    // MARK:  Segues
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "goToBlue" {
+            _ = segue.destinationViewController as? ViewController
+            
+        }
+        
+        if segue.identifier == "goToPink" {
+            _ = segue.destinationViewController as? ViewController
+        }
+        
+        if segue.identifier == "goToPinkToYellow" {
+            _ = segue.destinationViewController as? ViewController
+        }
+        if segue.identifier == "goToBlueToYellow" {
+            _ = segue.destinationViewController as? ViewController
+        }
+     }
+    
+    
+    
 }
 
